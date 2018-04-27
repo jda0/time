730d57ebf5770ca933bd866c015738cf2fd5b226
James D <jaydaly@connect.ust.hk>
2018-04-27 15:49:26 +0800

    Simplified

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const activeWin = require('active-win')
 const colors = require('colors')
 const moment = require('moment')
 
-let aLast = '';
-let aMost = '';
-let tLast = 0;
-let tMost = 0;
-let tUpdate = null;
+const MAX_INTERVAL = 60000
 
-const next = (reset = false) => {
+let aLast = ''
+let tLast = 0
+let tUpdate = null
+
+const next = () => {
   // let n = desktopIdle.getIdleTime();
   // if (n >= 5) {
   //   update(null, n * 1000)
@@ -18,60 +18,29 @@ const next = (reset = false) => {
     activeWin().then(w => {
       update(w.owner.name);
 
-      if (!reset) console.log(moment().format('HH:mm:ss').yellow, 
-        aLast.cyan, tLast.toString().grey,
-        (aMost !== aLast && aMost !== '' ? aMost.magenta : ''), 
-        (aMost !== aLast && aMost !== '' ? tMost.toString().gray : ''))
-
-      if (reset) {
-        aLast = ''
-        aMost = ''
-        tLast = 0
-        tMost = 2000
-      }
-
-      handle = setTimeout(next, tMost - 500)
+      handle = setTimeout(next, Math.min(tLast - 500, 60000))
     })
   // }
 }
 
 const update = (a) => {
-  const m = moment()
-  const t = m.diff(tUpdate)
-  tUpdate = m
+  const tOld = tUpdate
+  tUpdate = moment()
+  
+  if (tOld === null) return
+
+  const t = tUpdate.diff(tOld)
 
   if (a === aLast) {
-    tLast += t;
-    if (tLast > tMost){
-      if (aLast !== aMost) {
-        tMost = tLast
-        aMost = aLast
-      } else {
-        tMost += tLast
-      }
-    }
-  } else {
-    if (tLast > tMost) {
-      if (aLast !== aMost) {
-        tMost = tLast
-        aMost = aLast
-      } else {
-        tMost += tLast
-      }
-    }
+    tLast += t
 
+  } else {
+    console.log(moment().format('HH:mm:ss').yellow,
+        tLast === 0 ? 'Started' : `${Math.round(tLast / 1000)}s`.grey, '→', a.cyan)
     aLast = a
     tLast = 1000
   }
 }
 
 let handle
-next(true)
-
-setInterval(() => {
-  next(true)
-  console.log(moment().format('HH:mm:ss').black.bgYellow, 
-    aMost.black.bgWhite, tMost.toString().gray.bgWhite,
-    (aMost !== aLast ? aLast.blue : ''), 
-    (aMost !== aLast ? tLast.toString().gray : ''))
-}, 60000)
+next()
